d73c82fc4bd5092e62306596a0cf5d92ed039472
Sophie Graham <sophiergraham1@gmail.com>
2019-01-22 12:17:03 -0800

    turning in

--- test/app.test.js
+++ test/app.test.js
@@ -1,8 +1,44 @@
-const request = require("supertest");
-const app = require("../lib/app");
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe("Pirates API", () => {
-  it("write a test...", () => {
-    expect(true);
-  });
+describe('Penguins tests', () => {
+    it('gets penguins names in an array', () => {
+        return request(app)
+            .get('/')
+            .then(res => {
+                expect(res.text).toEqual(['bernice', 'bernard']);
+            });
+    });
+
+    it('gets a penguin', () => {
+        return request(app)
+            .post('/api/penguin/king?format=<simple|full>')
+            .then(res => {
+                expect(res.text).toEqual({
+                    name: 'bernice',
+                    description: 'What a penguin!',
+                    age: 7
+                });
+            });
+    });
+
+    it('deletes a penguin', () => {
+        return request(app)
+            .delete('/mistake')
+            .then(res => {
+                expect(res.text).toEqual(
+                    { deleted: true }
+                );
+            });
+    });
+
+    it('returns a 404', () => {
+        return request(app)
+            .get('/notHere')
+            .then(res => {
+                expect(res.status).toEqual(404);
+                expect(res.body).toEqual({ error: 'Not Found' });
+            });
+    });
 });
+  
