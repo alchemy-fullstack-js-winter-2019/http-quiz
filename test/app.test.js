3896fc643f6fc523ff61f4bfb822496d9f7ac127
JeiWolfei <jvampy22@gmail.com>
2019-01-22 12:10:13 -0800

    ugh penguin quiz is a mess

--- test/app.test.js
+++ test/app.test.js
@@ -1,8 +1,37 @@
-const request = require("supertest");
-const app = require("../lib/app");
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe("Pirates API", () => {
-  it("write a test...", () => {
-    expect(true);
-  });
+describe('Pirates API', () => {
+    it('write a test...', () => {
+        expect(true);
+    });
 });
+
+describe('Penguins Api', () => {
+    it('can get penguin from id', () => {
+        return request(app)
+            .get('/penguin')
+            .then(res => {
+                expect(res.body).toEqual({
+                    name: 'bernice',
+                    description: 'What a penguin!',
+                    age: '7'
+                });
+            });
+
+    it('can delete a penguin', () => {
+        return createPenguin('bernice')
+            .then(({ body }) => {
+            return request(app)
+                .delete(`/penguin/${body._id}`);
+            })
+            .then(({ body }) => {
+            expect(body).toEqual({ deleted: 1 });
+            });
+        });
+    });
+
+    });
+});
+
+
