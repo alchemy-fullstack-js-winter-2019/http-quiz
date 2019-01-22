c5889eb6132df5ada16a22e81fcd470e3fc0677a
Lance Owen Merrill <Lanceom@hawaii.edu>
2019-01-22 12:04:18 -0800

    finished

--- test/app.test.js
+++ test/app.test.js
@@ -9,4 +9,35 @@ describe('Pirates API', () => {
                 expect(res.body).toEqual(['bernice', 'bernard']);
             });
     });
+
+
+
+    it('can get the KING penguin', () => {
+        return request(app)
+            .get('/api/penguin/king?format=full')
+            .then(res => {
+                expect(res.body).toEqual({
+                    name: 'bernice',
+                    description: 'What a penguin!',
+                    age: 7
+                });
+            });
+    });
+
+    it('can return the KING`s name if something from URL is missing', () => {
+        return request(app)
+            .get('/api/penguin/')
+            .then(res => {
+                expect(res.body).toEqual({
+                    name: 'bernice'
+                });
+            });
+    });
+    it('can delete a penguin', () => {
+        return request(app)
+            .del('/mistake')
+            .then(res => {
+                expect(res.body).toEqual({ deleted: true });
+            });
+    });
 });
