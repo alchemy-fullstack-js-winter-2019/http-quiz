83bf1b3f7c5de7b2cb2e1d76dd56dd64ae24cef0
Lance Owen Merrill <Lanceom@hawaii.edu>
2019-01-22 11:28:39 -0800

    init commit

--- test/app.test.js
+++ test/app.test.js
@@ -1,8 +1,12 @@
-const request = require("supertest");
-const app = require("../lib/app");
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe("Pirates API", () => {
-  it("write a test...", () => {
-    expect(true);
-  });
+describe('Pirates API', () => {
+    it('can get a list of all penguins', () => {
+        return request(app)
+            .get('/api/penguins')
+            .then(res => {
+                expect(res.body).toEqual(['bernice', 'bernard']);
+            });
+    });
 });
