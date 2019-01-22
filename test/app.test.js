73b40b6f3154c2725715b0b87d63d0524a432bd2
Carmen Perezchica <crperezchica@gmail.com>
2019-01-22 12:22:17 -0800

    submitting test

--- test/app.test.js
+++ test/app.test.js
@@ -1,8 +1,50 @@
-const request = require("supertest");
-const app = require("../lib/app");
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe("Pirates API", () => {
-  it("write a test...", () => {
-    expect(true);
-  });
+// const createdPenguin = (penguin) => {
+//     return request(app)
+//         .post('/penguins/')
+//         .send({
+//             name: name,
+//             description: description,
+//             age: age
+//         })
+//         .then(res => res.body);
+// };
+
+describe('Pirates or penguins API', () => {
+    it('returns penguins or pirates', () => {
+        // const penguinNames = ['bernice', 'bernard'];
+        return request(app)
+            .get('/penguins');
+    })
+        .then(res => {
+            expect(res.text).toEqual(['bernice', 'bernard']);
+
+        });
+    it('format full', () => {
+        return request(app)
+            .get('/penguins/king?format=full');
+    })
+        .then(res => {
+            expect(res.body).toEqual({            
+                name: 'bernice',
+                description: 'What a penguin!',
+                age: 7 });
+        });
+    it('format simple', () => {
+        return request(app)
+            .get('/penguins/king?format=simple');
+    })
+        .then(res => {
+            expect(res.body).tContain({ name: 'bernice' });
+        });
+    it('deletes mistake', () => {
+        return request(app)
+            .deletes('/penguins/mistake');
+    })
+        .then(res => {
+            expect(res.body).toEqual({ deleted: 'true' });
+        });
 });
+
