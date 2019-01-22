a429699310a7499329fcfb05907a1fc81f01fa5d
Cari Palazzolo <cari.pizza@gmail.com>
2019-01-22 11:39:38 -0800

    GET /api/penguin/king?format=<simple|full> responds with json, test passes too

--- test/app.test.js
+++ test/app.test.js
@@ -10,4 +10,31 @@ describe('penguins API', () => {
         expect(res.body).toEqual(['bernice', 'bernard']);
       });
   });
+
+  // GET /api/penguin/king?format=<simple|full>
+  it('can conditionally respond with json per path', () => {
+    return request(app)
+      .get('/api/penguin/king?format=full')
+      .then(res => {
+        expect(res.body).toEqual({ 
+          name: 'bernice',
+          description: 'What a penguin!',
+          age: 7
+        });
+      })
+      .then(() => {
+        return request(app)
+          .get('/api/penguin/king?format=simple')
+          .then(res => {
+            expect(res.body).toEqual({ 
+              name: 'bernice'
+            });
+          });
+      });
+  });
+
+  // it('', () => {
+
+  // });
+
 });
