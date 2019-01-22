3667fecd471de51f32190f593cd50edcd549d727
Carmen V Ramos <carmenvramos@yahoo.com>
2019-01-22 12:01:29 -0800

    Added Query path test

--- test/app.test.js
+++ test/app.test.js
@@ -5,9 +5,24 @@ describe('Penguins API', () => {
     it('gets penguins', () => {
         return request(app)
             .get('/api/penguins')
-            .then(({ body }) => {
-                expect(body).toEqual(['bernice', 'bernard]']);
+            .then((res => {
+                expect(res.body).toEqual(['bernice', 'bernard]']);
         
+            }));
+    });      
+    
+
+    it(('gets a single penguin with query format=full', () => {
+        return request(app)
+            .get('/api/penguins/king?format=full')
+            .then(({ body }) => {
+                const result = { 
+                    name: 'bernice',
+                    description: 'What a penguin!',
+                    age: 7
+                };
+                expect(body).toEqual(result);
             });
-    });
+    }));      
+
 });
