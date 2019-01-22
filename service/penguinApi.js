72f15074b5749e3e6ddf1628c6af333f044cecad
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-22 12:28:11 -0800

    refactoring api

--- service/penguinApi.js
+++ service/penguinApi.js
@@ -2,6 +2,7 @@
 const request = require('supertest');
 
 const getPenguin = id => {
+    const penguin = JSON.stringify(id);
     return request
         .then(res => ({
             name: res.body.name,
