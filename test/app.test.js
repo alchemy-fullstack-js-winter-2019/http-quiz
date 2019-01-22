25a1bbf6f140a8e3491f0121bb6cd1bfc1e50c33
Marcy Silverman <marcysilve225@gmail.com>
2019-01-22 11:38:07 -0800

    inital set up and test done

--- test/app.test.js
+++ test/app.test.js
@@ -1,8 +1,24 @@
-const request = require("supertest");
-const app = require("../lib/app");
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe("Pirates API", () => {
-  it("write a test...", () => {
-    expect(true);
-  });
+const createPenguin = (name) => {
+    return request(app)
+        .post('/api/penguins')
+        .send({
+            name: name,
+        })
+        .then(res => res.body);
+};
+describe('Pirates API', () => {
+    it('get penguins...', () => {
+        const namesToCreate = ['bernice', 'bernard'];
+        return  Promise.all(namesToCreate.map(createPenguin))
+            .then(() => {
+                return request(app)
+                    .post('/api/penguins')
+                    .then(res => {
+                        expect(res.body).toEqual(['bernice', 'bernard']);
+                    });
+            });
+    });
 });
