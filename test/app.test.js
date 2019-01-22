f65238acce7a9ced3ee9801b0b16ca6e6b4903cc
Kristin Hortsch <kristinhortsch@gmail.com>
2019-01-22 11:52:52 -0800

    whoops forgot to be making commits...still working on the query string

--- test/app.test.js
+++ test/app.test.js
@@ -1,8 +1,40 @@
-const request = require("supertest");
-const app = require("../lib/app");
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe("Pirates API", () => {
-  it("write a test...", () => {
-    expect(true);
-  });
+describe('Penguins API', () => {
+    it('can get a list of penguins', () => {
+        return request(app)
+            .get('/api/penguins')
+            .then((res) => {
+                console.log(res.body);
+                expect(res.body).toHaveLength(2);
+            });
+    });
+    it('can get a penguin in the full format', () => {
+        return request(app)
+            .get('/api/penguins/king?format=full')
+            .then(res => {
+                expect(res.body).toEqual({
+                    name: 'bernice',
+                    description: 'What a penguin!',
+                    age: 7
+                });
+            });
+    });
+    it('can get a penguin in the simple format', () => {
+        return request(app)
+            .get('/api/penguins/king?format=simple')
+            .then(res => {
+                expect(res.body).toEqual({
+                    name: 'bernice'
+                });
+            });
+    });
+    it('can delete a penguin', () => {
+        return request(app)
+            .delete('/mistake')
+            .then(res => {
+                expect(res.body).toEqual({ deleted: true });
+            });
+    });
 });
