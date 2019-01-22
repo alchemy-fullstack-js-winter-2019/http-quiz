854fef2d8f047c8a312933bfcbdbc82ec227eb6c
Cari Palazzolo <cari.pizza@gmail.com>
2019-01-22 11:30:44 -0800

    GET /api/penguins returns ["bernice", "bernard"]

--- test/app.test.js
+++ test/app.test.js
@@ -1,8 +1,13 @@
-const request = require("supertest");
-const app = require("../lib/app");
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe("Pirates API", () => {
-  it("write a test...", () => {
-    expect(true);
+describe('penguins API', () => {
+  // GET /api/penguins should return ['bernice', 'bernard']
+  it('can respond with json', () => {
+    return request(app)
+      .get('/api/penguins')
+      .then(res => {
+        expect(res.body).toEqual(['bernice', 'bernard']);
+      });
   });
 });
