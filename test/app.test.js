21160e266cd2b38d6cf85f8db8553662f18b0c9e
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-22 11:36:22 -0800

    setting up tests

--- test/app.test.js
+++ test/app.test.js
@@ -1,8 +1,44 @@
-const request = require("supertest");
-const app = require("../lib/app");
+const request = require('supertest');
+const app = require('../lib/app');
+const mkdirp = require('mkdirp');
+const rimraf = require('rimraf');
 
-describe("Pirates API", () => {
-  it("write a test...", () => {
-    expect(true);
-  });
+const createPenguin = (name) => {
+    return request(app)
+        .post('/penguins')
+        .send({
+            name,
+            age: 7,
+            description: 'What a penguin!'
+        })
+        .then(res => res.body);
+};
+
+describe('Penguins API', () => {
+    beforeAll(done => {
+        mkdirp('./data/penguins', done);
+        done();
+    });
+    afterEach(done => {
+        rimraf('./data/penguins/*', done);
+        done();
+    });
+    afterAll(done => {
+        createPenguin('bernice');
+        return request(app)
+            .post('/penguins')
+            .send({
+                name: 'bernice',
+                age: 7,
+                description: 'What a penguin!'
+            })
+            .then(res => {
+                res.body;
+                done();
+            });
+    });
+    
+    it('write a test...', () => {
+        expect(true);
+    });
 });
