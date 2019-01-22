621c2a77c9ec9af2077ce7e2e9f6726ff62b5c23
Connor Frendt <cjacksonfx@yahoo.com>
2019-01-22 12:11:35 -0800

    trying to get all info on one penguin test to pass

--- test/app.test.js
+++ test/app.test.js
@@ -2,16 +2,27 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('Penguins API', () => {
-    it('can give the names of penguins', (body) => {
+    it('can give the names of penguins', () => {
         return request(app)
             .get('/penguins')
             .then(res => {
-                expect(body).toEqual(
+                expect(res.body).toEqual(
                     ['bernice', 'bernard']
                 );
 
             });
     });
 
-
+    it('can get all info on one penguin' () => {
+      return request(app)
+        .get('/penguin/king?format=<full>')
+        .then(res => {
+          expect(res.body).toEqual(
+            {
+              name: 'bernice',
+              description: 'What a penguin!',
+              age: 7
+          });
+        })
+    })
 });
