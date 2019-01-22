f4b79cf13ddae80f573f197dd1b04f9e076089ce
Tyler Corbett <thetylercorbett@gmail.com>
2019-01-22 12:05:59 -0800

    delete method working

--- test/app.test.js
+++ test/app.test.js
@@ -1,12 +1,23 @@
 const request = require("supertest");
 const app = require("../lib/app");
 
+
+
+
+
 describe("Penguins API", () => {
   it('returns an array of penguin names', () => {
     return request(app)
       .get('/api/penguins')
       .then(res => {
         expect(res.body).toHaveLength(2);
+      })
+  })
+  it('returns { deleted: true }', () => {
+    return request(app)
+      .delete('/mistake')
+      .then(res => {
+        expect(res.body).toEqual({ deleted: true });
       })
   })
 });
